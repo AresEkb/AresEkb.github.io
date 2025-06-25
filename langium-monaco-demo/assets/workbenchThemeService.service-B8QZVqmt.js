c9a2cdbd5701388360ece7b45e45521f3c12781b
Denis Nikiforov <denis.nikif@gmail.com>
2025-06-25 16:22:47 +0300

    Update Langium Monaco demo

--- langium-monaco-demo/assets/workbenchThemeService.service-B8QZVqmt.js
+++
@@ -1,1 +0,0 @@
-import{bW as e,h4 as r}from"./monaco-vscode-base-service-override-BYqBQsid.js";const c=e(r);export{c as I};
---
+++ langium-monaco-demo/assets/workbenchThemeService.service-DfpJQoWP.js
@@ -0,0 +1,1 @@
+import{bX as e,h6 as r}from"./monaco-vscode-base-service-override-DWrDktfJ.js";const c=e(r);export{c as I};
